cc80b2073896a1e6ec42311c58874ab7fa0737be
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-08-27 09:55:26 -0300

    nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const btnMenu = document.querySelector("#menu");
 const btnCerrarMenu = document.querySelector("#btnCerrarMenu");
 const nav = document.querySelector("#nav");
+const linksNav = document.querySelectorAll("#navList li a");
 
-btnMenu.addEventListener("click", ()=>{nav.classList.add("showNav")})
-btnCerrarMenu.addEventListener("click", ()=>{nav.classList.remove("showNav")})
+
+btnMenu.addEventListener("click", ()=>{nav.classList.add("showNav")});
+
+btnCerrarMenu.addEventListener("click", ()=>{nav.classList.remove("showNav")});
+
+linksNav.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("showNav")
+    });
+  });
+
+
